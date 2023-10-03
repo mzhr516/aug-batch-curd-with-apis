5cb2bfa89eb83598df24ecb6c7e3fd578ae62d67
mohdmazharuddin <106807606+mohdmazharuddin@users.noreply.github.com>
2023-10-03 16:07:27 +0530

    complete crud operations

--- src/components/userTable.jsx
+++ src/components/userTable.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Button, Table } from "react-bootstrap";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 export const UserTable = () => {
+  const { register, handleSubmit, reset } = useForm();
   const [usersData, setUsersData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [refetch, setRefetch] = useState(false);
+  const [editUser, setEditUser] = useState({});
 
   useEffect(() => {
     setLoading(true);
@@ -27,10 +30,29 @@ export const UserTable = () => {
       }
     });
   };
+
+  const onEdit = (user) => {
+    setEditUser(user);
+    reset(user);
+  };
+
+  const submit = (data) => {
+    console.log(data);
+    axios
+      .put(`http://localhost:3000/users/${editUser.id}`, data)
+      .then((res)=>{
+        if(res){
+            setEditUser({})
+            setRefetch(!refetch)
+        }
+      })
+      .catch();
+  };
+
   return (
     <div>
       {loading === true ? (
-        "Loading....."
+        <h1>Loading.....</h1>
       ) : (
         <Table striped bordered hover>
           <thead>
@@ -44,18 +66,59 @@ export const UserTable = () => {
           </thead>
           <tbody>
             {usersData.map((user, index) => {
-              return (
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>{user.name}</td>
-                  <td>{user.age}</td>
-                  <td>{user.address}</td>
-                  <td>{user.gender}</td>
-                  <td>
-                    <Button onClick={() => onDelete(user.id)}>Delete</Button>
-                  </td>
-                </tr>
-              );
+              if (user.id === editUser.id) {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td colSpan="5">
+                      <form onSubmit={handleSubmit(submit)}>
+                        <td>
+                          <input type="text" {...register("name")} />
+                        </td>
+                        <td>
+                          <input type="age" {...register("age")} />
+                        </td>
+                        <td>
+                          <input type="text" {...register("address")} />
+                        </td>
+                       
+                        {/* <td>
+                          <input type="text" />
+                        </td> */}
+                        <td>
+                          <Button onClick={() => setEditUser({})}>
+                            cancel
+                          </Button>
+                          {"  "}
+                          <Button type="submit">save</Button>
+                        </td>
+                      </form>
+                    </td>
+                  </tr>
+                );
+              } else {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>
+                      <Link to={`user-view/${user.id}`}>{user.name}</Link>{" "}
+                    </td>
+                    <td>{user.age}</td>
+                    <td>{user.address}</td>
+                    <td>{user.gender}</td>
+                    <td>
+                      <Button onClick={() => onDelete(user.id)}>Delete</Button>{" "}
+                      <Button
+                        onClick={() => {
+                          onEdit(user);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              }
             })}
           </tbody>
         </Table>
--- src/pages/UserView.jsx
+++ src/pages/UserView.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const UserView = (props) => {
-  return <div>UserView</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [userDetail, setUserDetail] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/users/${id}`)
+      .then((responce) => {
+        setUserDetail(responce.data);
+      })
+      .catch();
+  }, []);
+  const click = () => {
+    navigate(-1);
+  };
+  return (
+    <div>
+      <h1>Name: {userDetail.name}</h1>
+      <h2>Age: {userDetail.age}</h2>
+      <h3>address: {userDetail.address}</h3>
+      <h4>gender: {userDetail.gender}</h4>
+
+      <Button onClick={click}>back</Button>
+    </div>
+  );
 };
